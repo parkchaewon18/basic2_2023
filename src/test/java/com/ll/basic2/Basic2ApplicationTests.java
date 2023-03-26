33002f2c357061b0e6a6714e4ed6bee50ac2ce58
parkchaewon18 <0101angela@naver.com>
2023-03-26 15:58:00 +0900

    세팅

---
+++ src/test/java/com/ll/basic2/Basic2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.basic2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Basic2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
